7df0f91cd2f3d3b6b426791e08d02c6fdf56ee0e
Газинур Казбеков <kxkb@ya.ru>
2022-03-20 21:55:16 +0500

    fix

--- game_oop.py
+++ game_oop.py
@@ -1,40 +1,11 @@
 """
 
 """
-class Thing:
-
-    def __init__(self, thing_name, thing_protection, thing_attack, thing_hp):
-        self.thing_name = thing_name
-        self.thing_protection = thing_protection
-        self.thing_attack = thing_attack
-        self.thing_hp = thing_hp
-    
-
-class Person:
-     
-    def __init__(self, name, hp, base_attack, base_protection):
-        self.name = name
-        self.hp = hp
-        self.base_attack = base_attack
-        self.base_protection = base_protection
 
 
-    def set_things(things):
-        pass
+def main():
+    print('None')
 
 
-    def minis_hp(main):
-        pass
-
-class Paladin(Person):
-
-    def __init__ (self, name, hp, base_attack, base_protection):
-        super().__init__(name, base_attack)
-        self.hp = hp * 2
-        self.base_protection = base_protection * 2
-
-class Warrior(Person):
-
-    def __init__ (self, name, hp, base_attack, base_protection):
-        super().__init__(name, hp, base_protection)
-        self.base_attack = base_attack * 2
+if __name__ == "__main__":
+    main()
